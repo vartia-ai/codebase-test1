6f997ce453629078cf25e55cfdd8c78a4832da9f
al <>
2024-01-05 11:46:03 -0800

    Added 1 and 2 line function prototypes and functions.

--- main.c
+++ main.c
@@ -5,11 +5,29 @@
 #include "stuff.h"
 #include "things.h"
 
+// Single line prototype for one-line function. Prototype should not be included itself as a function in 
+// code_extraction.py.
+float one_line_func(float x);
+
+// Two line prototype for a two-line function. Prototype should not be included itself as a function in 
+// code_extraction.py. 
+float two_line_func(
+    float x
+);
+
 int main() 
 {
     public_stuff_func1();
     public_stuff_func2();
     public_things_func1();
     public_things_func2();
+    one_line_func();
     return 0;
 }
+
+// Single line function. This should be included as a function in code_extraction.py.
+float one_line_func(float x) { return x; }
+
+// Two line function. This should be included as a function in code_extraction.py.
+float two_line_func(
+    float x) { return x; };
