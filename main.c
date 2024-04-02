420745a427976c20018f1ae2d2017172d1f91916
Brian Williams <github@chromaticgray.com>
2024-04-02 12:29:24 -0700

    spacer

--- main.c
+++ main.c
@@ -1,78 +1,37 @@
-// main.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+ struct i3p
+    {
+        int i[3];
+        char *p;
+        long l;
+    };
 
-#include "header_func.h"
-#include "stuff.h"
-#include "things.h"
-#include "complex_types.h"
-
-#include <sanitizer/asan_interface.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-
-typedef unsigned int uint_t;
-
-char* hexstring_to_buffer(const char* hexstring) {
-    size_t len = strlen(hexstring);
-    size_t buffer_len = len / 2;
-    char* buffer = malloc(buffer_len + 1);
-    
-    for (size_t i = 0; i < buffer_len; i++) {
-        sscanf(hexstring + 2 * i, "%2hhx", &buffer[i]);
+    void fn_taking_array_of_struct_i3p(struct i3p array_of_i3p[])
+    {
+        char *p = "spacer";
+        return;
     }
-    
-    buffer[buffer_len] = '\0';
-    return buffer;
-}
 
-// Used to test printig the the data passed into functions
-void data_function(char *data, uint_t len);
+    int main(void)
+    {
+        struct i3p i3p_inst;
+        i3p_inst.i[0] = 1;
+        i3p_inst.i[1] = 2;
+        i3p_inst.i[2] = 3;
+        char *i3p_p = "shazbot";
+        i3p_inst.p = i3p_p;
+        i3p_inst.l = 0xdeadbeef;
 
-// Single line prototype for one-line function. Prototype should not be included itself as a function in 
-// code_extraction.py.
-float one_line_func(float x);
+        struct i3p i3p_inst2;
+        i3p_inst2.i[0] = 4;
+        i3p_inst2.i[1] = 5;
+        i3p_inst2.i[2] = 6;
+        char *i3p_p2 = "bezurb";
+        i3p_inst2.p = i3p_p2;
+        i3p_inst2.l = 0xfeedface;
 
-// Two line prototype for a two-line function. Prototype should not be included itself as a function in 
-// code_extraction.py. 
-float two_line_func(
-    float x
-);
+        struct i3p array_of_i3p[2];
+        array_of_i3p[0] = i3p_inst;
+        array_of_i3p[1] = i3p_inst2;
 
-int main() 
-{
-    public_stuff_func1();
-    public_stuff_func2();
-    public_things_func1();
-    public_things_func2();
-    float r1 = one_line_func(4.0);
-    float r2 = two_line_func(5.0);
-    printf("r1=%f, r2=%f\n", r1, r2);
-    const char* hexstring = "8048656C6C6F20576F726C64"; // Hex representation of "Hello World" but with an invalid starting byte
-    char* buffer = hexstring_to_buffer(hexstring);
-    uint_t len = strlen(buffer);
-    data_function(buffer, len);
-    free(buffer);
-    test_struct_trace();
-    return 0;
-}
-
-// Single line function. This should be included as a function in code_extraction.py.
-float one_line_func(float x) { return x; }
-
-// Two line function. This should be included as a function in code_extraction.py.
-float two_line_func(
-    float x) { return one_line_func(x); };
-
-void data_function(char *data, uint_t len) {
-    printf("data_function: %s\n", data);
-    for (float i=0; i<10; i++) {
-        one_line_func(i);
+        fn_taking_array_of_struct_i3p(array_of_i3p);
     }
-    for (float i=0; i<10; i++) {
-        two_line_func(i);
-    }
-}
